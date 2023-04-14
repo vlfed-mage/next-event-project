3c692f88c8e73ce4a050ff3669b02ac90cec17e7
Valdo <gkorchun@gmail.com>
2023-04-14 16:14:42 +0300

    Extracting filtered data

--- src/pages/events/[...slug].tsx
+++ src/pages/events/[...slug].tsx
@@ -1,6 +1,35 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import { DateFilter } from '@/global/types';
+
+import { getFilteredEvents } from '@/data/mock-data';
+import EventsList from '@/components/events/events-list';
 
 export default function FilteredEventsPage() {
+    const router = useRouter();
+    let dateFilter: DateFilter | null = null;
+
+    if (Array.isArray(router.query.slug) && router.query.slug.length === 2) {
+        const [year, month] = router.query.slug;
+        dateFilter = {
+            year: +year,
+            month: +month,
+        };
+    }
+    if (dateFilter) {
+        const { year, month } = dateFilter;
+        if (year > 2030 || year < 2021 || month < 1 || month > 12) {
+            return <p>Invalid filter. Please adjust your values!</p>;
+        }
+    }
+
+    const filteredEvents = dateFilter ? getFilteredEvents(dateFilter) : [];
+
+    if (!filteredEvents || filteredEvents.length === 0) {
+        return <p>No events found for chosen filter!</p>;
+    }
+
     return (
         <>
             <Head>
@@ -9,7 +38,7 @@ export default function FilteredEventsPage() {
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <link rel='icon' href='/favicon.ico' />
             </Head>
-            <h1>Filtered events page</h1>
+            <EventsList events={filteredEvents} />
         </>
     );
 }
